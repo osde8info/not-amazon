12dd30feb8747c657effd307dd80d4566e596ad9
clive darra <cdarra@localhost.localdomain>
2020-12-19 21:41:17 +0000

    m

--- pages/index.js
+++ pages/index.js
@@ -5,47 +5,36 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>not amazon</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://www.not-amazon.ca/">not amazon</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>XXXX &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://www.not-amazon.ca/" className={styles.card}>
+            <h3>not amazon</h3>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="https://www.not-amazon.ca/" className={styles.card}>
+            <h3>not amazon</h3>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            href="https://www.not-amazon.ca/" className={styles.card}>
+            <h3>not amazon</h3>
+          </a>
+
+          <a
+            href="https://www.not-amazon.ca/" className={styles.card}>
+            <h3>not amazon</h3>
           </a>
         </div>
       </main>
