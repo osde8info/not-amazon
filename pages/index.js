16ed35008028aca80bd89c1baee4772053574041
clive darra <cdarra@localhost.localdomain>
2020-12-19 22:15:29 +0000

    m

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,6 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          not amazon shopping
-        </h1>
-
         <h2 className={styles.title}>
           not amazon CANADA
         </h2>
